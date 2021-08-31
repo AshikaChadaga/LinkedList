9b0b3868e5a8ca1d514dd38f4f774b3104a2011e
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-31 16:57:15 +0530

    REFACTORED: used deleteWithKey method

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -10,12 +10,14 @@ public class LinkedListMain {
 		MyNode<Integer> secondNode = new MyNode<Integer>(30);
 		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
 		MyNode<Integer> nodeToBeInserted = new MyNode<Integer>(40);
-		int valueOfPreviousNode = 30;
+		int valueOfPreviousNode = 70;
 		LinkedList myLinkedList = new LinkedList();
 		myLinkedList.add(firstNode);
 		myLinkedList.append(thirdNode);
 		myLinkedList.insert(firstNode, secondNode);
 		myLinkedList.insertWithKey(valueOfPreviousNode, nodeToBeInserted);
+		INode deletedNode = myLinkedList.deleteNodeWithKey(40);
+		System.out.println("The deleted key value is:"+deletedNode.getKey());
 		myLinkedList.printLinkedList();
 				
 	}
