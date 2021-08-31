4052714d03dd13018a0946810871670850aaa2ca
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-31 14:31:54 +0530

    REFACTORED: created linked list using append

--- src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -5,6 +5,7 @@ public class LinkedListMain {
 	public static void main(String[] args) {
 		
 		System.out.println("----- Welocme to Data Structure: Linked List -----");
+		System.out.println("Linked list 1 using add");
 		MyNode<Integer> firstNode = new MyNode<Integer>(70);
 		MyNode<Integer> secondNode = new MyNode<Integer>(30);
 		MyNode<Integer> thirdNode = new MyNode<Integer>(56);
@@ -13,5 +14,15 @@ public class LinkedListMain {
 		myLinkedList.add(secondNode);
 		myLinkedList.add(thirdNode);
 		myLinkedList.printLinkedList();
+		
+		System.out.println("Linked list 1 using append");
+		MyNode<Integer> Node1 = new MyNode<Integer>(56);
+		MyNode<Integer> Node2 = new MyNode<Integer>(30);
+		MyNode<Integer> Node3 = new MyNode<Integer>(70);
+		LinkedList myLinkedList1 = new LinkedList();
+		myLinkedList1.append(Node1);
+		myLinkedList1.append(Node2);
+		myLinkedList1.append(Node3);
+		myLinkedList1.printLinkedList();
 	}
 }
