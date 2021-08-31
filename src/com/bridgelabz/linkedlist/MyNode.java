89e1f733c4075d36a45f8795f0e8c9be668a5421
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-31 12:43:16 +0530

    REFACTORED: created sample Linked List

--- src/com/bridgelabz/linkedlist/MyNode.java
+++ src/com/bridgelabz/linkedlist/MyNode.java
@@ -1,9 +1,32 @@
 package com.bridgelabz.linkedlist;
 
 public class MyNode<K> {
-
+	
+	private K key;
+	private MyNode next;
+	
+	public MyNode(K key) {
+		
+		this.key = key;
+		this.next = null;
+	}
+	
+	public MyNode getNext() {
+		return next;
+	}
+	
+	public void setNext(MyNode next) {
+		this.next = next;
+	}
+	
 	public static void main(String[] args) {
 		
-		System.out.println("----- Welcome to DataStructure: LinkedList -----");
+		System.out.println("----- Welcome to Data Structure: Linked List -----");
+		
+		MyNode<Integer> firstNode = new MyNode<Integer>(56);
+		MyNode<Integer> secondNode = new MyNode<Integer>(30);
+		MyNode<Integer> thirdNode = new MyNode<Integer>(70);
+		firstNode.setNext(secondNode);
+		secondNode.setNext(thirdNode);
 	}
 }
