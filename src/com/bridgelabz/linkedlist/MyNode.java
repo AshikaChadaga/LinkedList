a22403665aae332f8da1d136311aa79d3c23a39d
AshikaChadaga <ashikachadaga@gmail.com>
2021-08-31 12:39:52 +0530

    ADDED: Welcome Message

---
+++ src/com/bridgelabz/linkedlist/MyNode.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.linkedlist;
+
+public class MyNode<K> {
+
+	public static void main(String[] args) {
+		
+		System.out.println("----- Welcome to DataStructure: LinkedList -----");
+	}
+}
